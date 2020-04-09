49c24794b9971bb3137057b971effbed32b199ae
Kelvin <kelvin.martinez92@gmail.com>
2020-04-09 19:39:12 -0400

    heroku push

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var databaseToUse = ""
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static('client/build'));
+	databaseToUse = "mongodb://gabe:a123456@ds133231.mlab.com:33231/heroku_5vfx1n8w";
+}
+else {
+	databaseToUse = 'mongodb://localhost/reactBoilerplate';
+}
+
+
+app.use(routes);
+
+const MONGODB_URI = process.env.MONGODB_URI || databaseToUse;
+
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(MONGODB_URI);
+
+app.listen(PORT, function() {
+	console.log(`App running on port ${PORT}`);
+});
